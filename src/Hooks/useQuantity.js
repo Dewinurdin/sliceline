a958e98d9da6a96210219b72ebd27961a7aeb6ad
Dewinurdin <dewinurdin@gmail.com>
2020-05-24 15:03:50 -0500

    add quantity input and hook

---
+++ src/Hooks/useQuantity.js
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+
+export function useQuantity(defaultQuantity){
+  const [value, setValue] = useState(defaultQuantity || 1);
+
+  function onChange(e){
+    // convert the value to number with + Operator
+      // and if the number is Not Greater than 1
+    if( !(+e.target.value >= 1)){
+      // we are setting default to value 1
+      setValue(1);
+      return;
+    }
+    setValue(+e.target.value);
+  }
+  return {
+    value,
+    setValue,
+    onChange
+  }
+}
